a5f15277e24e0dc33e4d72e2931e29adb801ca8a
haifeng2777 <107272252+haifeng2777@users.noreply.github.com>
2024-12-04 00:30:41 +0800

    Update 金牌影院.js

--- js/金牌影院.js
+++ js/金牌影院.js
@@ -1,143 +1,56 @@
-// http://localhost:5757/api/金牌影院?ac=list&t=1&pg=1
-// http://localhost:5757/api/金牌影院?ac=detail&ids=/detail/131374
-// http://localhost:5757/api/金牌影院?wd=我的&pg=1
-// http://localhost:5757/api/金牌影院?play=&flag=金牌影院
 var rule = {
-    类型: '影视',
     title: '金牌影院',
-    desc: '金牌影院纯js版本',
-    host: 'https://m.cfkj86.com/',
-    homeUrl:'https://www.cfkj86.com',
-    url: 'https://m.cfkj86.com/api/mw-movie/anonymous/video/list?pageNum=fypage&pageSize=30&sort=1&sortBy=1&type1=fyclass',
+    host: 'https://www.cfkj86.com',
+    url: '/vod/show/id/fyclass/page/fypage',
     searchUrl: '/api/mw-movie/anonymous/video/searchByWordPageable?keyword=**&pageNum=fypage&pageSize=12&type=false',
+    headers: {
+        'User-Agent': 'PC_UA',
+        'Referer': 'https://www.cfkj86.com/'
+    },
     searchable: 2,
     quickSearch: 0,
-    timeout: 5000,
+    filterable: 0,
+    class_name: '电影&电视剧&综艺&动漫',
+    class_url: '1&2&3&4',
+    limit: 6,
+    double: false,
     play_parse: true,
-    class_parse: async () => {
-         let classes = [{
-                type_id: '1',
-                type_name: '电影',
-            },{
-                type_id: '2',
-                type_name: '剧集',
-            },{
-                type_id: '3',
-                type_name: '综艺',
-            },{
-                type_id: '4',
-                type_name: '动漫',
-            }];
-        return {
-            class: classes,
-        }
-    },
-    预处理: async () => {
-        return []
-    },
-    推荐: async () => {
-        return []
-    },
-    一级: async function (tid, pg, filter, extend) {
-        let {MY_CATE, input} = this;
-        if (pg <= 0) pg = 1;
-        const t = new Date().getTime()
-        const signkey = `pageNum=${pg}&pageSize=30&sort=1&sortBy=1&type1=${tid}&key=cb808529bae6b6be45ecfab29a4889bc&t=`+t
-        const key = CryptoJS.SHA1(CryptoJS.MD5(signkey).toString()).toString()
-        const html = JSON.parse((await req(`https://m.cfkj86.com/api/mw-movie/anonymous/video/list?pageNum=${pg}&pageSize=30&sort=1&sortBy=1&type1=${tid}`,
-            {
-            headers:{
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-                'Accept': 'application/json, text/plain, */*',
-                'deviceId': misc.randUUID(),
+    lazy: $js.toString(() => {
+        let url_id = input.split('/')[5];
+        let jishu_id = input.split('/')[7];
+        let t = new Date().getTime();
+        eval(getCryptoJS);
+        let signkey = 'id=' + url_id + '&nid=' + jishu_id + '&key=cb808529bae6b6be45ecfab29a4889bc&t=' + t;
+        let key = CryptoJS.SHA1(CryptoJS.MD5(signkey).toString()).toString();
+        let json_data = JSON.parse(request('https://www.cfkj86.com/api/mw-movie/anonymous/video/episode/url?id=' + url_id + '&nid=' + jishu_id, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+                'deviceid': '58a80c52-138c-48fd-8edb-138fd74d12c8',
                 'sign': key,
                 't': t
             }
-        })).content);
-        let d = [];
-        const list = html.data.list
-        log(list)
-        list.forEach((it)=>{
-            d.push({
-                title: it.vodName,
-                url: '/detail/'+it.vodId,
-                desc: it.vodRemarks || it.vodVersion,
-                pic_url: it.vodPic,
-            })
-        })
-        return setResult(d)
-    },
-    二级: async function (ids) {
-        log(this)
-        let {input} = this;
-        const html = (await req(`${input}`)).content;
-        const $ = pq(html)
-        const vod = {
-            vod_id: input,
-            vod_name: $('h1').text().trim(),
-        };
-        let playFroms = [];
-        let playUrls = [];
-        const temp = [];
-        let playlist=$('div.main-list-sections__BodyArea-sc-8bb7334b-2 .listitem')
-        for (const it of playlist) {
-            const a = $(it).find('a')[0]
-            temp.push(a.children[0].data+'$'+a.attribs.href)
+        }));
+        let url = json_data.data;
+        log(url);
+        if (url) {
+            input = {parse: 0, url: url, header: rule.headers};
         }
-        playFroms.push('不知道倾情打造');
-        playUrls.push(temp.join('#'));
-        vod.vod_play_from = playFroms.join('$$$');
-        vod.vod_play_url = playUrls.join('$$$');
 
-        return vod
+    }),
+    图片替换: $js.toString(() => {
+        // log(MY_URL);
+        let src = decodeURIComponent(input).split(',')[0].split(' ')[0];
+        input = urljoin(MY_URL, src) + '@Referer=https://www.cfkj86.com/';
+    }),
+    // double: false,
+    // 推荐: '*',
+    一级: '.movie-ul&&.kIDbTD;.title&&Text;.card-img&&img&&srcset;.info-tag&&Text;.kIDbTD a&&href',
+    二级: {
+        title: 'h1&&Text',
+        img: 'img&&src',
+        tabs: '.top a',
+        lists: '.kmkeNw:eq(#id)&&a',
     },
-    搜索: async function (wd, quick, pg) {
-        let {input} = this
-        const t = new Date().getTime()
-        //keyword=你&pageNum=1&pageSize=12&type=false&key=cb808529bae6b6be45ecfab29a4889bc&t=1722904806016
-        const signkey = 'keyword='+wd+'&pageNum='+pg+'&pageSize=12&type=false&key=cb808529bae6b6be45ecfab29a4889bc&t='+t
-        const key = CryptoJS.SHA1(CryptoJS.MD5(signkey).toString()).toString()
-        let html = JSON.parse((await req(`${rule.homeUrl}${input}`,
-            {
-                headers:{
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-                    'Accept': 'application/json, text/plain, */*',
-                    'deviceId': misc.randUUID(),
-                    'sign': key,
-                    't': t
-                }
-        })).content);
-        let d = [];
-        const list = html.data.list
-        list.forEach((it)=>{
-            d.push({
-                title: it.vodName,
-                url: '/detail/'+it.vodId,
-                desc: it.vodRemarks || '暂无更新',
-                pic_url: it.vodPic,
-            })
-        })
-        return setResult(d)
-    },
-    lazy: async function (flag, id, flags) {
-        let {input} = this;
-        const pid = input.split('/')[3]
-        const nid = input.split('/')[5]
-        const t = new Date().getTime()
-        const signkey = 'id='+pid+'&nid='+nid+'&key=cb808529bae6b6be45ecfab29a4889bc&t='+t
-        const key = CryptoJS.SHA1(CryptoJS.MD5(signkey).toString()).toString()
-        const relurl = rule.homeUrl+'/api/mw-movie/anonymous/v1/video/episode/url?id='+pid+'&nid='+nid
-        const html = JSON.parse((await req(relurl,
-            {
-                headers:{
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-                    'Accept': 'application/json, text/plain, */*',
-                    'deviceId': misc.randUUID(),
-                    'sign': key,
-                    't': t
-                }
-        })).content)
-        return {parse: 0, url: html.data.playUrl, js: ''}
-    },
-};
+    搜索: '*',
+}
 
